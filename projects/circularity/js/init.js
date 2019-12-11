68355c65711b6bc7e148880f9d8cdc99000d9442
mystacj <jackson.mysta@gmail.com>
2019-12-11 19:45:30 +0000

    <update12-10>

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,31 +1,31 @@
 var init = function (window) {
     'use strict';
-    var 
+    var
         draw = window.opspark.draw,
         physikz = window.opspark.racket.physikz,
-        
+
         app = window.opspark.makeApp(),
-        canvas = app.canvas, 
+        canvas = app.canvas,
         view = app.view,
         fps = draw.fps('#000');
-        
-    
+
+
     window.opspark.makeGame = function() {
-        
+
         window.opspark.game = {};
         var game = window.opspark.game;
-        
+
         ////////////////////////////////////////////////////////////////
         // ALL CODE GOES BELOW HERE                                   //
         ////////////////////////////////////////////////////////////////
-        
+
         // TODO 1 : Declare and initialize our variables
 
 var circle;		    // variable to hold a single circle when creating circles / iterating
 var circles = [];	// variable to store all circles in one Array
 
-        // TODO 2 : Create a function that draws a circle 
-        
+        // TODO 2 : Create a function that draws a circle
+
 function drawCircle() {
     circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
     physikz.addRandomVelocity(circle, canvas);
@@ -34,337 +34,86 @@ function drawCircle() {
 }
 
 
-        // TODO 3 / 7 : Call the drawCircle function 
-        
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-    
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();    
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        
-        
-  
-        view.addChild(fps);
-        app.addUpdateable(fps);
-    
-        game.checkCirclePosition = function(circle) {
-            // TODO 5 : YOUR CODE STARTS HERE //////////////////////
-            
-            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            } 
-            // TODO 5a) if the circle has gone past of the LEFT side of the screen then place it on the RIGHT
-            else if ( / * Fill me in! * / ) {
-                
-            } 
+        // TODO 3 / 7 : Call the drawCircle function
 
-            // TODO 5b) if the circle has gone past of the TOP side of the screen then place it on the BOTTOM
-            if ( / * Fill me in! * / ) {
-                
+            // drawCircle();
+            // drawCircle();
+            // drawCircle();
+            // drawCircle();
+            // drawCircle();
+            var loopsCompleted = 0;
+            while (loopsCompleted < 100) {
+                drawCircle();
+                loopsCompleted++
             }
-            // TODO 5c) if the circle has gone past of the BOTTOM side of the screen then place it OFF-SCREEN TOP
-            else if ( / * Fill me in! * / ) {
-            
-            }
-            // YOUR TODO 5 CODE ENDS HERE //////////////////////////
-        }
-    
+
+
         function update() {
             // TODO 4 : Update the circle's position //
 
-                function update() {
-	// TODO 4 : Update the circle's position //
+        // physikz.updatePosition(circles[0]  );
+        // physikz.updatePosition(circles[1]  );
+        // physikz.updatePosition(circles[2]  );
+        // physikz.updatePosition(circles[3]  );
+        // physikz.updatePosition(circles[4]  );
 
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-}
-            
             // TODO 5 : Call game.checkCirclePosition on your circles.
-           
-    game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+
+        // game.checkCirclePosition(circles[0] );
+        // game.checkCirclePosition(circles[1] );
+        // game.checkCirclePosition(circles[2] );
+        // game.checkCirclePosition(circles[3] );
+        // game.checkCirclePosition(circles[4] );
 
             // TODO 8 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+            var eachValue = circles[i];
+
+                physikz.updatePosition(eachValue)
+                game.checkCirclePosition(eachValue)
+            // code to repeat using eachValue
+        }
 
         }
-        
+
+
+        game.checkCirclePosition = function(circle) {
+            // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            if ( circle.x > canvas.width ) {
+                circle.x = 0;
+            }
+            // TODO 5a) if the circle has gone past of the LEFT side of the screen then place it on the RIGHT
+            else if (circle.x < 0 ) {
+                circle.x = canvas.width
+            }
+
+            // TODO 5b) if the circle has gone past of the TOP side of the screen then place it on the BOTTOM
+            else if (circle.y > canvas.height ) {
+                circle.y = 0
+            }
+            // TODO 5c) if the circle has gone past of the BOTTOM side of the screen then place it OFF-SCREEN TOP
+            else if (circle.y < 0  ) {
+                circle.y = canvas.height
+            }
+
+            // YOUR TODO 5 CODE ENDS HERE //////////////////////////
+        }
+
+
+
         ////////////////////////////////////////////////////////////////////
         // NO CODE BELOW HERE                                             //
         ////////////////////////////////////////////////////////////////////
-        
+        view.addChild(fps);
+        app.addUpdateable(fps);
+
         game.circle = circle;
         game.circles = circles;
         game.drawCircle = drawCircle;
         game.update = update;
-        
+
         app.addUpdateable(window.opspark.game);
     }
 };
